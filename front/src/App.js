afe6471867d3aa7dbfd177102846361cb5a5f415
antonin <antonin.girard.98@gmail.com>
2022-02-13 20:19:27 +0100

    :sparkles: init front

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import ReactDOM from 'react-dom';
 
 function App() {
+  const client = new ApolloClient({
+      cache: new InMemoryCache(),
+      uri: "http://localhost:3001/"
+    })
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    
+    
+    ReactDOM.render(
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </ApolloProvider>,
+      document.getElementById('root'),
+    )
+    
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
